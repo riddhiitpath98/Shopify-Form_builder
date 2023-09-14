dee92c4db08b948d32253534daaff8fcab6cf697
riddhiitpath98 <riddhip.itpathsolutions@gmail.com>
2023-09-14 18:09:14 +0530

    extra changes added

--- web/index.js
+++ web/index.js
@@ -17,25 +17,28 @@ const PORT = 3007;
 //   10
 // );
 
+// `${process.cwd()}/frontend/dist`
+console.log(process.env.NODE_ENV, "+++");
 
-const STATIC_PATH = `${process.cwd()}/frontend/dist`
+const STATIC_PATH = `${process.cwd()}/frontend/`
 // process.env.NODE_ENV === "production"
 //   ? `${process.cwd()}/frontend/dist`
 //   : `${process.cwd()}/frontend/`;
 
+console.log(STATIC_PATH, "STATIC_PATH");
 
 const app = express();
 app.use(cors());
 
 //for CORS
-app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "*");
-  req.header("Content-Type", "application/json");
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+//   res.setHeader("Access-Control-Allow-Headers", "*");
+//   req.header("Content-Type", "application/json");
+//   res.setHeader("Access-Control-Allow-Credentials", true);
+//   next();
+// });
 
 // let httpServer;
 // const privateKey = fs.readFileSync('certy/privkey.pem', 'utf8');
